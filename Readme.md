ef8921c17eb7a68444a89b7be1bd1c5ac61cf676
novice <david@cninone.com>
2019-08-11 23:20:30 +0800

    add udp things

--- Readme.md
+++ Readme.md
@@ -1,7 +1,26 @@
- Install docker on your public server, and then run this script
+ ## Signaling Service docker image
+ 
+ Install docker on your public server, and then run
+ ```bash
+    docker run --restart=always -d -p 57000:57000 -p 3478:3478 -p 49152-65535:49152-65535/udp novice/ss
+ ```   
+ Or run it as a swarm service
  ```bash
     #!/bin/bash
     if [ "$(docker info | grep Swarm | sed 's/Swarm: //g')" == "inactive" ]; then
         docker swarm init
     fi
     docker stack deploy -c stack.yml ss
+```
+
+### About the opened ports
+This image contain coturn service, so it need 3478/tcp and those udp ports opened for exchanging media. 57000/tcp is for websocket signaling service.
+
+### Client app
+https://play.google.com/store/apps/details?id=freenet.cppsvr
+
+### What this app can do?
+1. Start a http server on android device for clients using browser upload, download files or streaming media. [done]
+2. Start a socks proxy on android device with PAC file for using it. [done]
+3. Webrtc client for video/audio chat using your own server with this image. [todo]
+4. Obiter: Qr/Barcode scan
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,15 +1,30 @@
-
+const https = require('https');
+const dgram = require('dgram');
 const WebSocket = require('ws');
 const _ = require('lodash');
-
-const port = isNaN(process.argv[2]) ? 57000 : process.argv[2];
-const wss = new WebSocket.Server({ port }, () => {
+const ssl = require('./ssl');
+const port = process.env.PORT || 57000; //isNaN(process.argv[2]) ? 50000 : process.argv[2];
+const udp = dgram.createSocket('udp4');
+const server = https.createServer({
+    cert: ssl.crt,
+    key: ssl.key
+});
+const wss = new WebSocket.Server({ server });
+server.listen(port, () => {
     console.log(`service listen on ${port}`)
 });
+
 const peers = {}
 function get_peers_by_size(size = 100) {
+    const chosen = _.sampleSize(Object.keys(peers), size)
+    const ps = _.map(chosen, pid=>{
+        return{
+            id: pid,
+            ep: peers[pid].ep
+        }
+    })
     return {
-        peers: _.sampleSize(Object.keys(peers), size),
+        peers: ps,
         cmd: 'peers'
     };
 }
@@ -24,40 +39,45 @@ function broadcast_total() {
 }
 wss.on('connection', ws => {
     ws.is_alive = true;
-    ws.on('close', message => {
+    ws.on('close', () => {
         delete peers[ws.pid]
         broadcast_total()
     })
     ws.on('message', message => {
         try {
+            console.log(message)
             const data = JSON.parse(message)
+            console.log(data)
             switch (data.cmd) {
                 case 'peer_online': {
                     clearTimeout(ws.idle_tm)
                     const ps = get_peers_by_size();
+                    console.log(ps)
                     ws.pid = data.id;
-                    peers[data.id] = ws;
+                    peers[data.id] = {
+                        ws,
+                        token: data.token
+                    };
                     ws.send(JSON.stringify(ps))
                     broadcast_total()
                     break;
                 }
                 case 'need_peers': {
                     const p = get_peers_by_size(data.amount);
-                    p.peers = _.filter(p.peers, p => p != ws.pid)
+                    p.peers = _.filter(p.peers, p => p.id != ws.pid)
                     ws.send(JSON.stringify(p))
                     break;
                 }
                 case 'send_sig':
                 case 'return_sig': {
-                    peers[data.to].send(message);
+                    peers[data.to].ws.send(message);
                     break;
                 }
-                case 'I_am_alive':
-                    ws.is_alive = true;
-                    break;
+
                 default: throw 'not support cmd'
             }
         } catch (err) {
+            console.log(err)
             ws.terminate()
         }
     });
@@ -75,3 +95,30 @@ const interval = setInterval(() => {
         send_total(ws)
     });
 }, 30000);
+// udp
+udp.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    // udp.close();
+});
+
+udp.on('message', (msg, rinfo) => {
+    // console.log(`udp got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    // udp.send('echo from server', rinfo.port, rinfo.address)
+    if( msg.readUInt8() == 0x18 && msg.length == 9 ){
+        const id = msg.readInt32BE(1)
+        const token = msg.readUInt32BE(5)
+        if( peers.hasOwnProperty(id) ){
+            if(peers[id].token == token){
+                peers[id].ep = `${rinfo.address}:${rinfo.port}`
+                peers[id].ws.is_alive = true;
+            }
+        }
+    }
+});
+
+udp.on('listening', () => {
+    // const address = udp.address();
+    // console.log(`udp server listening ${address.address}:${address.port}`);
+});
+
+udp.bind(port);
